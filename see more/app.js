f639bd1022a5e9cb0f3e51e56f4469ab1469d63d
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-06-29 16:50:47 +0500

    Assignment 8

--- see more/app.js
+++ see more/app.js
@@ -1,18 +1,15 @@
 const para = document.querySelector("#para");
 const text = para.innerHTML;
-para.innerText = `${para.innerHTML.slice(0,97)}`;
-
-
 const button = document.querySelector("#button");
-button.innerHTML = "...show more";
 
+para.innerText = `${para.innerHTML.slice(0,97)}`;
+button.innerHTML = "...show more";
 
 const show = () => {
     para.innerHTML = text;
     button.innerHTML = "...Hide";
     button.setAttribute("onclick","hide()");
 }
-
 
 const hide = () => {
     para.innerHTML = `${para.innerHTML.slice(0,97)}`
@@ -21,4 +18,3 @@ const hide = () => {
 }
 
 button.setAttribute("onclick","show()");
-
