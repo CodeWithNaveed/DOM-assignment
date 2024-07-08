623bdeb35be1f9c539fae322d7adcdaf7c32c0b9
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-07-08 17:11:56 +0500

    assignment 8

---
+++ frequently ask question/app.js
@@ -0,0 +1,29 @@
+let item = document.getElementsByClassName("container");
+let show = document.getElementsByClassName("show");
+let hide = document.getElementsByClassName("hide");
+
+Array.from(item).forEach((item, i) => {
+    item.addEventListener("click", () => {
+        let res = item.classList.toggle("active");
+        if(res){
+            show[i].style.display = "none";
+            hide[i].style.display = "block"; 
+        } else {
+            show[i].style.display = "block";
+            hide[i].style.display = "none";
+        }
+    });
+});
+
+// for(let i=0; i<item.length; i++){
+//     item[i].addEventListener("click", () =>{
+//         let res = item[i].classList.toggle("active");
+//         if(res == true){
+//             show[i].style.display="none";
+//             hide[i].style.display="block";
+//         }else{
+//             show[i].style.display="block";
+//             hide[i].style.display="none";
+//         }
+//     })
+// }
