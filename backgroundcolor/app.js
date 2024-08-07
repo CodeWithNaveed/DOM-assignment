1ff23e02d2add7dd74327bf05856e513f025e429
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-08-08 02:06:09 +0500

    assignment 1 modified

--- backgroundcolor/app.js
+++ backgroundcolor/app.js
@@ -1,7 +1,12 @@
-const color =["green","blue","black","yellow","red"]
+let number = document.querySelector(".number")
+let bg = document.querySelector("body");
+let color =["green","blue","black","yellow","red"]
+
 function colorchanger (){
-    const numRandom = Math.floor(Math.random() * color.length)
-    console.log(numRandom)
-    const result = document.querySelector(".heading")
-    result.style.backgroundColor = color[numRandom];
+    const numRandom = Math.floor(Math.random() * color.length);
+    bg.style.backgroundColor = color[numRandom];
+    number.textContent = color[numRandom];
+    number.style.color = color[numRandom];
 }
+
+colorchanger ()
